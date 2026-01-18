45cb377b2c20e151f6ada420498955d3cdca3cfa
CodeZx6 <131226257+CodeZx6@users.noreply.github.com>
2026-01-18 20:49:35 +1100

    Delete ST-CSL directory

--- ST-CSL/src/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from .loader import SpatioTemporalDataLoader, load_pretrained_weights, set_random_seed
-
-__all__ = [
-    'SpatioTemporalDataLoader',
-    'load_pretrained_weights',
-    'set_random_seed'
-]
--- ST-CSL/src/training/__init__.py
+++
@@ -1,7 +0,0 @@
-from .pretrain import ContrastivePretrainer
-from .evaluate import STCSLEvaluator
-
-__all__ = [
-    'ContrastivePretrainer',
-    'STCSLEvaluator'
-]
